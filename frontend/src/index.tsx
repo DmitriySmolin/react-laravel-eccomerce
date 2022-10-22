60a9a3f5bf11199014964e1f98379b5cae3573d3
Dmitry <neox56@gmail.com>
2022-10-22 20:35:23 +0400

    Add support scss and managing scss files

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../src/assets/css/custom.css';
-import '../src/assets/css/fontawesome.css';
+import '../src/assets/css/custom.scss';
+import '../src/assets/css/fontawesome.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
