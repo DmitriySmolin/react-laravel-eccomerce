b5bee1eb1908ed77c423554841fe937d64b8ebba
Dmitry <neox56@gmail.com>
2022-10-29 15:16:45 +0400

    Add slick slider

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,11 +2,13 @@ import React, {Fragment} from 'react';
 import FeaturedProducts from "../components/home/FeaturedProducts";
 import Collection from "../components/home/Collection";
 import Categories from "../components/home/Categories";
+import NewArrival from "../components/home/NewArrival";
 
 const HomePage = () => {
     return (
         <Fragment>
             <FeaturedProducts/>
+            <NewArrival/>
             <Collection/>
             <Categories/>
         </Fragment>
