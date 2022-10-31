5fcd20556dd27d47d3857545c885654478afa1b8
Dmitry <neox56@gmail.com>
2022-10-31 23:02:02 +0400

    Add user login

--- frontend/src/route/AppRoute.tsx
+++ frontend/src/route/AppRoute.tsx
@@ -1,12 +1,14 @@
 import React, {Fragment} from 'react';
 import {Routes, Route} from 'react-router-dom';
 import HomePage from "../pages/HomePage";
+import UserLoginPage from "../pages/UserLoginPage";
 
 const AppRoute = () => {
     return (
         <Fragment>
             <Routes>
                 <Route path="/" element={<HomePage/>}/>
+                <Route path="/login" element={<UserLoginPage/>}/>
             </Routes>
         </Fragment>
     );
