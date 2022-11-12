129bba0e020ff69b7ae653b95d24eebae01f5b2d
Dmitry <neox56@gmail.com>
2022-11-13 01:46:04 +0400

    create visitor details Rest API

--- frontend/src/api/AppURL.tsx
+++ frontend/src/api/AppURL.tsx
@@ -1,4 +1,4 @@
-class AppURL {
+ class AppURL {
     static baseURL = "http://127.0.0.1:8000/api"
     static visitorDetails = this.baseURL + "/getvisitor"
     static postContact = this.baseURL + "/postcontact"
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -9,11 +9,22 @@ import NavMenuMobile from "../components/common/NavMenuMobile";
 import HomeTopMobile from "../components/home/HomeTopMobile";
 import FooterDesktop from "../components/common/FooterDesktop";
 import FooterMobile from "../components/common/FooterMobile";
+import axios from "axios";
+import AppURL from "../api/AppURL";
 
 const HomePage = () => {
+
     useEffect(() => {
         window.scroll(0, 0);
+        getVisitorDetails();
     }, [])
+
+   const getVisitorDetails = () => {
+        axios.get(AppURL.visitorDetails).then().catch();
+    }
+
+
+
     return (
         <Fragment>
             <div className="Desktop">
